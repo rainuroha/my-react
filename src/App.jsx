d7a88d2a161c0ab887e533375011a24b265c0b52
rainurohra@gmail.com <rainurohra@gmail.com>
2026-03-13 03:37:11 -0300

    this is my initial my-react-app

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import  React from "react";
+
+
+function App(){
+  
+
+const myList = (
+  <ul>
+    <li>Learn React</li>
+    <li>Practice JSX</li>
+    <li>Build Projects</li>
+    <li>Become a Developer</li>
+  </ul>
+ 
+  
+);
+return myList;
+}
+
+
+export default App;
